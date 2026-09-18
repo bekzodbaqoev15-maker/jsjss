39bc461b7d177d9fd32c48595ed968f226792d85
bekzodbaqoev15-maker <bekzodbaqoev15@gmail.com>
2026-09-18 15:08:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let text = prompt("Text yoz:");
+alert(typeof text);
+
+let number = Number(prompt("Number yoz:"));
+alert(typeof number);
+
+let bigNumber = BigInt(prompt("BigInt yoz:"));
+alert(typeof bigNumber);
+
+let symbol = Symbol(prompt("Symbol yoz:"));
+alert(typeof symbol);
+
+let bool = Boolean(prompt("Boolean yoz:"));
+alert(typeof bool);
+
